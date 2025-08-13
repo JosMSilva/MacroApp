ee98a8065fc5e2b381036ea6649bfb288781b15e
JosMSilva <jm.silva@ua.pt>
2025-08-13 15:45:15 +0100

    Update

--- src/app/dashboard/dashboard.ts
+++ src/app/dashboard/dashboard.ts
@@ -18,12 +18,19 @@ export class Dashboard {
   targetCalories = signal(2781);
   consumedCalories = signal(1200);
   macros = signal<Macro[]>([
-    { label: 'Protein', color: '#ff7b6e', consumed: 2, target: 140 },
-    { label: 'Fat',     color: '#f1c40f', consumed:  65, target:  92 },
-    { label: 'Carbs',   color: '#2ecc71', consumed: 200, target: 346 },
+  { label: 'Protein', color: '#ff7b6e', consumed: 95, target: 140 },
+  { label: 'Fat',     color: '#f1c40f', consumed:  65, target:  92 },
+  { label: 'Carbs',   color: '#2ecc71', consumed: 200, target: 346 },
+  //{ label: 'Saturated Fat',   color: '#f18f0fff', consumed: 20, target: 46 },
+  //{ label: 'Sugar',   color: '#2eccbfff', consumed: 28, target: 36 },
   ]);
   expenditure7d = signal<number[]>([2440, 2460, 2540, 2430, 2420, 2440, 2440]);
   weight7d      = signal<number[]>([62.4, 62.4, 62.4, 62.4, 62.4, 62.4, 62.4]);
+
+  energyBalance7d(): number[] {
+    const goal = this.targetCalories();
+    return this.expenditure7d().map(dayValue => goal - dayValue);
+  }
 
 
   /********************************* Signals *********************************/
@@ -66,19 +73,66 @@ export class Dashboard {
     return m.target > 0 ? value / m.target : 0;
   }
 
-  // tiny SVG sparkline path (keeps dependencies = 0)
-  spark(values: number[], width = 260, height = 64, pad = 8): string {
-    if (!values.length) return '';
+
+  /********************************* Create SVG with Week Data *********************************/
+
+  /******** Define The Scale of The Graphs ********/
+  getSparkScales(values: number[], width = 260, height = 64, pad = 8) {
     const min = Math.min(...values);
     const max = Math.max(...values);
     const span = Math.max(max - min, 1e-6);
-
     const stepX = (width - pad * 2) / Math.max(values.length - 1, 1);
-    const scaleY = (v: number) =>
-      height - pad - ((v - min) / span) * (height - pad * 2);
+    const scaleY = (v: number) => height - pad - ((v - min) / span) * (height - pad * 2);
 
-    return values
-      .map((v, i) => `${i ? 'L' : 'M'} ${pad + i * stepX},${scaleY(v)}`)
-      .join(' ');
+    return { min, max, span, stepX, scaleY };
   }
+
+  /******** Graph ********/
+  spark(values: number[], width = 260, height = 64, pad = 8): string {
+    if (!values.length) return '';
+    const { stepX, scaleY } = this.getSparkScales(values, width, height, pad);
+
+    return values.map((v, i) => `${i ? 'L' : 'M'} ${pad + i * stepX},${scaleY(v)}`).join(' ');
+  }
+
+  /******** Graph Dots ********/
+  sparkDots(values: number[], width = 260, height = 64, pad = 8) {
+    if (!values.length) return [];
+    const { stepX, scaleY } = this.getSparkScales(values, width, height, pad);
+
+    return values.map((v, i) => ({cx: pad + i * stepX,cy: scaleY(v)}));
+  }
+
+  /******** Graph Average Line ********/
+  sparkAvgLine(values: number[], width = 260, height = 64, pad = 8) {
+    if (!values.length) return null;
+    const { stepX, scaleY } = this.getSparkScales(values, width, height, pad);
+
+    const avg = values.reduce((a, b) => a + b, 0) / values.length;
+    const avgY = scaleY(avg);
+
+    return { y: avgY, x1: pad, x2: width - pad };
+  }
+
+  /******** Calculate Average ********/
+  getAverage(values: number[]): number {
+    if (!values.length) return 0;
+    return Math.round((values.reduce((a, b) => a + b, 0) / values.length) * 10) / 10;
+  }
+
+hours = [
+  { time: '09:00', event: 'Oat Meal' },
+  { time: '10:00', event: 'Protein Shake' },
+  { time: '11:00', event: '' },
+  { time: '12:00', event: 'Spaghetti Carbonara' },
+  { time: '13:00', event: '' },
+  { time: '14:00', event: 'Protein Bar' }
+];
+
+isCurrentHour(hour: string) {
+  const now = new Date();
+  const current = now.getHours().toString().padStart(2, '0') + ':00';
+  return current === hour;
+}
+
 }
